870af14701b90b9dc741769a3cb477fbaca63670
Michael Nogueira <michaelsnogueira@gmail.com>
2023-12-21 16:26:47 -0300

    Desafio03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,33 +3,18 @@ package org.example;
 import java.util.Scanner;
 
 
-//Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+//The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
 //
-//A byte is an 8-bit signed integer.
-//        A short is a 16-bit signed integer.
-//        An int is a 32-bit signed integer.
-//        A long is a 64-bit signed integer.
-//Given an input integer, you must determine which primitive data types are capable of properly storing that
+//Hint: Java's Scanner.hasNext() method is helpful for this problem.
 public class Main {
     public static void main(String[] argh) {
 
-
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        for (int i = 0; i < t; i++) {
-
-            try {
-                long x = sc.nextLong();
-                System.out.println(x + " can be fitted in:");
-                if (x >= -128 && x <= 127) System.out.println("* byte");
-                if (x >= -32768 && x <= 32767) System.out.println("* short");
-                if (x >= Math.pow(-2, 31) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
-                System.out.println("* long");
-            } catch (Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
-            }
-
+        int i = 0;
+        while(sc.hasNext())
+        {
+            System.out.println(++i +" "+ sc.nextLine());
         }
+
     }
 }

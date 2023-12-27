fd7e369e3e6f1440bb5a0e0c236aa3f88067b3b9
Michael Nogueira <michaelsnogueira@gmail.com>
2023-12-27 16:37:22 -0300

    Desafio04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,19 +2,30 @@ package org.example;
 
 import java.util.Scanner;
 
-
-//The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
 //
-//Hint: Java's Scanner.hasNext() method is helpful for this problem.
+//Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
+//
+//It's time to test your knowledge of Static initialization blocks. You can read about it here.
+//
+//You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth  and height . You should read the variables from the standard input.
+//
+//        If  or  , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
 public class Main {
+    private static int largura, altura;
+    private static boolean flag;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        largura = sc.nextInt();
+        altura = sc.nextInt();
+        flag = largura > 0 && altura > 0;
+    }
     public static void main(String[] argh) {
 
-        Scanner sc = new Scanner(System.in);
-        int i = 0;
-        while(sc.hasNext())
-        {
-            System.out.println(++i +" "+ sc.nextLine());
-        }
+        if (flag) {
+            System.out.println(largura * altura);
+        } else
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
 
     }
 }

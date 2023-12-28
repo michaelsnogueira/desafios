5fc05b7c2ee5aa10a3ebd757f14c6c62d9f82472
Michael Nogueira <michaelsnogueira@gmail.com>
2023-12-28 12:57:45 -0300

    Desafio05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,30 +2,20 @@ package org.example;
 
 import java.util.Scanner;
 
-//
-//Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
-//
-//It's time to test your knowledge of Static initialization blocks. You can read about it here.
-//
-//You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth  and height . You should read the variables from the standard input.
-//
-//        If  or  , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+//You are given an integer , you have to convert it into a string.
+//Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
+//can range between  to  inclusive.
 public class Main {
-    private static int largura, altura;
-    private static boolean flag;
-
-    static {
-        Scanner sc = new Scanner(System.in);
-        largura = sc.nextInt();
-        altura = sc.nextInt();
-        flag = largura > 0 && altura > 0;
-    }
     public static void main(String[] argh) {
 
-        if (flag) {
-            System.out.println(largura * altura);
-        } else
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        Scanner sc = new Scanner(System.in);
+        try {
+            int n = sc.nextInt();
+            String.valueOf(n);
+            System.out.println("Good job");
+        } catch (Exception e) {
+            System.out.println("Wrong answer");
+        }
 
     }
 }

193bdaff5d0534f2fcf06d0b8228489c9e7dec8c
Michael Nogueira <michaelsnogueira@gmail.com>
2024-01-02 16:37:19 -0300

    Desafio06

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,45 @@
 package org.example;
 
-import java.util.Scanner;
+import java.io.*;
+import java.time.LocalDateTime;
 
-//You are given an integer , you have to convert it into a string.
-//Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
-//can range between  to  inclusive.
+//You are given a date. You just need to write the method, , which returns the day on that date. To simplify your task, we have provided a portion of the code in the editor.
 public class Main {
-    public static void main(String[] argh) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        Scanner sc = new Scanner(System.in);
-        try {
-            int n = sc.nextInt();
-            String.valueOf(n);
-            System.out.println("Good job");
-        } catch (Exception e) {
-            System.out.println("Wrong answer");
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+    class Result {
+
+        /*
+         * Complete the 'findDay' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. INTEGER month
+         *  2. INTEGER day
+         *  3. INTEGER year
+         */
+
+        public static String findDay(int month, int day, int year) {
+        LocalDateTime localDateTime = LocalDateTime.of(year, month, day, 0, 0, 0, 0);
+        return localDateTime.getDayOfWeek().name();
         }
 
     }
